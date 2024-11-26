966cdbe4374e5335a01f8f7e719acd856f3eabc2
Negri234279 <rafalejo01@gmail.com>
2024-11-26 20:05:20 +0100

    Agrega la interfaz CharacterRepository para la gestión de personajes

---
+++ src/contexts/characters/@types/character-repository.d.ts
@@ -0,0 +1,9 @@
+import { ICharacter } from './character'
+
+export interface CharacterRepository<T = ICharacter> {
+    findBy(filter: Partial<T>): Promise<T[]>
+    findOneBy(filter: Partial<T>): Promise<T | null>
+    create(characters: T | T[]): Promise<void>
+    updateOne(filter: Partial<T>, character: Partial<T>): Promise<void>
+    delete(): Promise<void>
+}
