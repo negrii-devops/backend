2157966875870dc580a032f3f500571a16b74720
Negri234279 <rafalejo01@gmail.com>
2024-11-27 19:18:56 +0100

    Agrega un endpoint para servir avatares de personajes y actualiza la búsqueda de personajes para soportar paginación

--- src/contexts/characters/controllers/characters.controller.ts
+++ src/contexts/characters/controllers/characters.controller.ts
@@ -1,18 +1,35 @@
-import { Controller, Get, NotFoundException, Param, ParseIntPipe } from '@nestjs/common'
-import { CharactersRepositoryImpl } from '../repositories/characters.repository-impl'
+import { Controller, Get, NotFoundException, Param, Query, Res } from '@nestjs/common'
+import { Response } from 'express'
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+import { PaginationDto } from 'src/shared/dtos/pagination.dto'
 import { ICharacter } from '../@types/character'
+import { CharactersRepositoryImpl } from '../repositories/characters.repository-impl'
 
 @Controller('characters')
 export class CharactersController {
     constructor(private readonly charactersRepository: CharactersRepositoryImpl) {}
 
+    @Get('avatar/:filename')
+    async avatar(@Param('filename') filename: string, @Res() res: Response) {
+        const avatarPath = path.join(__dirname, '../../../../uploads/characters', filename)
+
+        const exists = fs.existsSync(avatarPath)
+        if (!exists) {
+            throw new NotFoundException('Avatar not found')
+        }
+
+        res.sendFile(avatarPath)
+    }
+
     @Get('')
-    async find(): Promise<ICharacter[]> {
-        return await this.charactersRepository.findBy({})
+    async find(@Query() pagination: PaginationDto): Promise<ICharacter[]> {
+        console.log(pagination)
+        return await this.charactersRepository.findBy({ ...pagination })
     }
 
     @Get(':id')
-    async findOne(@Param('id', ParseIntPipe) id: number): Promise<ICharacter> {
+    async findOne(@Param('id') id: string): Promise<ICharacter> {
         const character = await this.charactersRepository.findOneBy({ id })
         if (!character) {
             throw new NotFoundException('Character not found')
