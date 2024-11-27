1c083a74ca62485a3e5ff04c23ea7a06dd323f6f
Negri234279 <rafalejo01@gmail.com>
2024-11-27 19:09:31 +0100

    Configura CORS, seguridad con Helmet y versionado de API en el servidor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import helmet from 'helmet'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
-    await app.listen(process.env.PORT ?? 3000)
+
+    app.enableCors()
+    app.use(helmet())
+
+    app.setGlobalPrefix('api')
+    app.enableVersioning({
+        type: VersioningType.URI,
+        defaultVersion: '1',
+    })
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+            forbidNonWhitelisted: true,
+        }),
+    )
+
+    await app.listen(AppModule.port, '0.0.0.0')
+
+    console.log(`Server is running on: http://127.0.0.1:${AppModule.port}/api/v1`)
 }
 bootstrap()
